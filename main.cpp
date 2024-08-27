ac188ade54ae7fc267786ad5022c569ff3cb2dda
Humpers96 <alex.humphrey@hotmail.co.uk>
2024-08-27 23:30:26 +0100

    migrated to element buffers. goat is god.

--- main.cpp
+++ main.cpp
@@ -33,6 +33,10 @@ struct vertex
     float y;
     float z;
 
+    void print() const
+    {
+        std::cout << "x: " << x << ", y: " << y << ", z: " << z << std::endl;
+    }
     //rgba col;
 };
 
@@ -182,17 +186,41 @@ int main(int argc, char** argv)
     // OpenGL buffer/array objects
     // ---------------------
 
-    std::vector<vertex> verts
-    {
-        { -0.5f, -0.5f, 0.0f },
-        {  0.5f, -0.5f, 0.0f },
-        {  0.0f,  0.5f, 0.0f }
+    // std::vector<vertex> verts
+    // {
+    // // first triangle
+    //     { 0.5f,  0.5f, 0.0f }  // top right
+    //     { 0.5f, -0.5f, 0.0f }  // bottom right
+    //     {-0.5f,  0.5f, 0.0f }  // top left 
+    //     // second triangle 
+    //     { 0.5f, -0.5f, 0.0f }  // bottom right
+    //     {-0.5f, -0.5f, 0.0f }  // bottom left
+    //     {-0.5f,  0.5f, 0.0f }  // top left
+    // };
+
+    float vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
     };
 
-    // init vertex buffer obj & vertex array obj
-    GLuint VBO, VAO;
+    unsigned int indices[] = 
+    {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    // for (const auto& v : verts)
+    // {
+    //     v.print();
+    // }
+
+    // init vertex buffer, element buffer obj & vertex array obj
+    GLuint VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     // bind VAO first and then bind & set vertex buffers
     glBindVertexArray(VAO);
@@ -205,9 +233,13 @@ int main(int argc, char** argv)
     // type of buffer to copy into, size, data
     // STATIC - data set once, drawn many times
     // DYNAMIC - data set many times, drawn many times
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex) * 3, vec_addr(verts), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //glBufferData(GL_ARRAY_BUFFER, sizeof(vertex) * verts.size(), vec_addr(verts), GL_STATIC_DRAW);
     // at this point vertex data is stored on the gpu in memory managed by the vertex buffer VBO
 
+    // binds EBO to the GL_ELEMENT_ARRAY_BUFFER target
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     // OpenGL needs to know the context of the vertex data it is being fed
     // location of the vertex attribute to configure (location = 0)
@@ -217,9 +249,12 @@ int main(int argc, char** argv)
     // stride value between vertices
     // start position/offset of data in buffer
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
+    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertex), (void*)0);
     // each vertex attribute takes its data from memory managed by a VBO
     // which VBO it takes from is determined by which is currently bound to GL_ARRAY_BUFFER
     // when glVertexAttribPointer() is called
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     // enable the vertex attributes using vertex attribute location (location = 0)
     glEnableVertexAttribArray(0);
@@ -235,27 +270,60 @@ int main(int argc, char** argv)
     {
         glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        // glEnable(GL_CULL_FACE);
+        // glCullFace(GL_BACK);
+        // glFrontFace(GL_CW);
 
         // instructs OpenGL to use the specified shader program for any following rendering calls
         glUseProgram(shader_program);
+        // bind VAO to draw from
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // draw arrays
+        // OpenGL type to draw
+        // starting vertex index in array
+        // number of vertices to draw
+        //glDrawArrays(GL_TRIANGLES, 0, verts.size());
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // swap buffers
         SDL_GL_SwapWindow(sdl_window);
 
         SDL_Event ev;
 
         while (SDL_PollEvent(&ev))
         {
-            if (ev.type == SDL_QUIT)
+            switch (ev.type)
             {
-                malta = true;
-                break;
-            }
-
-            if (ev.key.keysym.sym == SDLK_ESCAPE)
-            {
-                malta = true;
-                break;
+                case SDL_KEYDOWN:
+                {
+                    switch (ev.key.keysym.sym)
+                    {
+                        case SDLK_ESCAPE:
+                        {
+                            malta = true;
+                            std::cout << "escape pressed" << std::endl;
+                            break;
+                        }
+                        default:
+                        {
+                            std::cout << "key pressed" << std::endl;
+                            break;
+                        }
+                    }
+                    break;
+                }
+                case SDL_MOUSEBUTTONDOWN:
+                {
+                    std::cout << "click" << std::endl;
+                    break;
+                }
+                case SDL_QUIT:
+                {
+                    malta = true;
+                    std::cout << "sdl quit called" << std::endl;
+                    break;
+                }
             }
         }
     }
@@ -265,6 +333,7 @@ int main(int argc, char** argv)
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shader_program);
 
     // SDL shutdown
